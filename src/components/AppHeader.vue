18cd1c61dd83b014ebafb677f0b8c90a64d6d559
TheAkshitS <Akshit.S@Outlook.com>
2020-05-24 01:13:08 +0530

    refactor: vuetify update

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar app color="success" dark>
+    <div class="d-flex align-center"></div>
+
+    <v-spacer />
+
+    <v-btn target="_blank" icon>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
