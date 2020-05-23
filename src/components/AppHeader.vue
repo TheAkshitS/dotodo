777bead4434841a85dd8fda577f6016280bdf26f
TheAkshitS <Akshit.S@Outlook.com>
2020-05-24 03:06:38 +0530

    refactor: add app name + update theme switcher

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,15 @@
 <template>
   <v-app-bar app color="success" dark>
-    <div class="d-flex align-center"></div>
+    <v-toolbar-title dark class="text-uppercase ">
+      <span class="font-weight-light">✔&nbsp;DO</span>
+      <span>&nbsp;It</span>
+    </v-toolbar-title>
 
     <v-spacer />
 
-    <v-btn target="_blank" icon>
-      <v-icon>mdi-open-in-new</v-icon>
+    <v-btn icon @click="$vuetify.theme.dark = !$vuetify.theme.dark">
+      <v-icon v-if="this.$vuetify.theme.dark">mdi-brightness-7</v-icon>
+      <v-icon v-else>mdi-brightness-4</v-icon>
     </v-btn>
   </v-app-bar>
 </template>
