4b3224c4faaf956cb11079aa49d655aa23a26259
TheAkshitS <Akshit.S@Outlook.com>
2020-05-25 07:46:47 +0530

    feat: global mixing for checking user logged in status

--- src/mixins/global.js
+++ src/mixins/global.js
@@ -2,6 +2,10 @@ export default {
   computed: {
     $_taskList() {
       return this.$store.state.task.tasks
+    },
+
+    $_isUserLoggedin() {
+      return this.$store.getters['user/isUserLoggedIn']
     }
   }
 }
